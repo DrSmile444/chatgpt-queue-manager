85cbf3d690686e0828f5bea4c5be3fe141a04494
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-11-25 14:26:05 +0200

    feat: add json support for queue input

--- src/main.js
+++ src/main.js
@@ -72,7 +72,7 @@ const queueTemplate = `
     </div>
     <p data-queue-total class="queue-description"></p>
     <ul class="queue-list" data-queue-list></ul>
-    <textarea class="queue-input" placeholder="Add message here" rows="4" data-queue-input></textarea>
+    <textarea class="queue-input" placeholder="Add message or JSON here" rows="4" data-queue-input></textarea>
     <button class="queue-button queue-button-primary queue-button-icon" data-queue-start-button>
       <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z M20,13c0-4.42-3.58-8-8-8c-0.06,0-0.12,0.01-0.18,0.01l1.09-1.09L11.5,2.5L8,6l3.5,3.5l1.41-1.41 l-1.08-1.08C11.89,7.01,11.95,7,12,7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02C16.95,20.44,20,17.08,20,13z"/></g></g></svg>
       Start
@@ -397,8 +397,19 @@ function addQueue() {
 
   queueInput.addEventListener('keydown', (event) => {
     if (event.key === 'Enter' && event.target.value) {
-      queueStartNumber += 1;
-      queueItems.push(event.target.value.trim());
+      const value = event.target.value.trim();
+
+      try {
+        const json = JSON.parse(value);
+        if (Array.isArray(json)) {
+          queueStartNumber += json.length;
+          queueItems.push(...json);
+        }
+      } catch {
+        queueStartNumber += 1;
+        queueItems.push(value);
+      }
+
       updateQueueList();
 
       setTimeout(() => {
