ea10b19e4cc5506faa45ddb5d5c502fdd9178a8a
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-11-25 13:26:08 +0200

    feat: add queue download / upload / clear functionality

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ console.log('Hello, ChatGPT!');
 
 const QUEUE_STATE_KEY = 'queueState';
 
-const queueItems = JSON.parse(localStorage.getItem(QUEUE_STATE_KEY) || 'null') || [
+let queueItems = JSON.parse(localStorage.getItem(QUEUE_STATE_KEY) || 'null') || [
   'Hi ChatGPT! What is the most recent date of the information you have access to?',
   'Simply answer 1',
   'Simply answer 2',
@@ -54,10 +54,26 @@ const queueTemplate = `
       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
     </button>
     <p class="queue-description">Add and organize your responses to send them in a sequential manner</p>
+    <input type="file" accept="application/json" data-upload-input style="display: none;" />
+    <div class="queue-buttons">
+      <div class="queue-buttons-left">
+        <button class="queue-button" data-upload-button title="Upload queue">
+          <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><path d="M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M7,9l1.41,1.41L11,7.83V16h2V7.83l2.59,2.58L17,9l-5-5L7,9z"/></g></svg>
+        </button>
+        <button class="queue-button" data-download-button title="Download queue">
+          <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><path d="M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M17,11l-1.41-1.41L13,12.17V4h-2v8.17L8.41,9.59L7,11l5,5 L17,11z"/></g></svg>
+        </button>
+      </div>
+      <div class="queue-buttons-right">
+        <button class="queue-button" data-clear-button title="Clear queue">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>
+        </button>
+      </div>
+    </div>
     <p data-queue-total class="queue-description"></p>
     <ul class="queue-list" data-queue-list></ul>
     <textarea class="queue-input" placeholder="Add message here" rows="4" data-queue-input></textarea>
-    <button class="queue-start-button" data-queue-start-button>
+    <button class="queue-button queue-button-primary queue-button-icon" data-queue-start-button>
       <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z M20,13c0-4.42-3.58-8-8-8c-0.06,0-0.12,0.01-0.18,0.01l1.09-1.09L11.5,2.5L8,6l3.5,3.5l1.41-1.41 l-1.08-1.08C11.89,7.01,11.95,7,12,7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02C16.95,20.44,20,17.08,20,13z"/></g></g></svg>
       Start
     </button>
@@ -130,17 +146,39 @@ const queueStyle = `
   border-radius: 6px;
 }
 
-.queue-start-button {
-  background: var(--queue-primary);
-  color: var(--surface-primary);
+.queue-button {
+  background: var(--surface-primary);
+  color: var(--queue-primary);
   font-weight: bold;
   padding: 4px;
-  padding-right: 28px;
   border-radius: 6px;
   display: flex;
   gap: 8px;
   justify-content: center;
   font-size: 16px;
+  border: 1px solid var(--queue-primary);
+}
+
+.queue-button-primary {
+  background: var(--queue-primary);
+  color: var(--surface-primary); 
+  border: none;
+}
+
+.queue-button-icon {
+  padding-right: 28px;
+}
+
+.queue-buttons {
+  display: flex;
+  gap: 8px;
+  justify-content: space-between;
+}
+
+.queue-buttons-left,
+.queue-buttons-right {
+  display: flex;
+  gap: 8px;
 }
 </style>
 `;
@@ -315,6 +353,14 @@ function addQueue() {
   const queue = queueTemplate.querySelector('[data-queue]');
   const queueInput = queueTemplate.querySelector('[data-queue-input]');
   const queueClose = queueTemplate.querySelector('[data-queue-close]');
+  /**
+   * @type {HTMLInputElement}
+   * */
+  const queueUploadInput = queueTemplate.querySelector('[data-upload-input]');
+  const queueUploadButton = queueTemplate.querySelector('[data-upload-button]');
+  const queueDownloadButton = queueTemplate.querySelector('[data-download-button]');
+  const queueClearButton = queueTemplate.querySelector('[data-clear-button]');
+
   queueStartButton = queueTemplate.querySelector('[data-queue-start-button]');
   queueList = queueTemplate.querySelector('[data-queue-list]');
   queueTotal = queueTemplate.querySelector('[data-queue-total]');
@@ -322,6 +368,28 @@ function addQueue() {
   queueClose.addEventListener('click', () => {
     queueShow.classList.remove('hide');
     queue.classList.add('hide');
+  });
+
+  queueUploadButton.addEventListener('click', () => {
+    queueUploadInput.click();
+  });
+
+  queueUploadInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    parseJsonFile(file).then((result) => {
+      queueItems = result;
+      queueStartNumber = queueItems.length;
+      updateQueueList();
+    });
+  });
+
+  queueDownloadButton.addEventListener('click', () => {
+    downloadFile(queueItems, 'queue-manager-' + Date.now());
+  });
+
+  queueClearButton.addEventListener('click', () => {
+    queueItems = [];
+    updateQueueList();
   });
 
   queueInput.addEventListener('keydown', (event) => {
@@ -400,6 +468,61 @@ function createQueueItem(text, index) {
 
   return queueItemTemplate;
 }
+
+function downloadFile(object, name = 'data') {
+  const blob = new Blob([JSON.stringify(object)], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+
+  const aElement = document.createElement('a');
+  aElement.href = url;
+  aElement.download = name + '.json'; // The filename for download
+  document.body.appendChild(aElement); // We append the anchor to the body for Firefox support
+  aElement.click(); // Simulate click to trigger download
+  document.body.removeChild(aElement); // Clean up
+  URL.revokeObjectURL(url); // Free up memory
+}
+
+/**
+ * @param {File | undefined} file
+ * */
+function parseJsonFile(file) {
+  return new Promise((resolve, reject) => {
+    // Check if file is provided
+    if (!file) {
+      reject("No file provided.");
+      return;
+    }
+
+    // Check if file type is JSON
+    if (!file.type === "application/json") {
+      reject("File is not a JSON.");
+      return;
+    }
+
+    const reader = new FileReader();
+
+    // On successful read
+    reader.onload = (event) => {
+      try {
+        // Parse the file content as JSON
+        const jsonObj = JSON.parse(event.target.result);
+        resolve(jsonObj);
+      } catch (error) {
+        // If an error occurs while parsing
+        reject(error);
+      }
+    };
+
+    // On read error
+    reader.onerror = (event) => {
+      reject(event.target.error);
+    };
+
+    // Read the file as text
+    reader.readAsText(file);
+  });
+}
+
 
 if (document.readyState === 'complete' || document.readyState === 'interactive') {
   queueInit();
